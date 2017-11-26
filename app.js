2636ba1e084a2d9a0be057e9c649f98c3f6b3569
leo ouyang <leo.ouyang@mail.utornto.ca>
2017-11-26 10:46:56 -0500

    testing session

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ const app = express();
 app.use(cookieParser());
 app.use(session({secret: "CSC309"}));
 
+var times = {}
 app.get('/', function(req, res){
-   if(req.session.page_views){
-      req.session.page_views++;
-	  req.session.save();
-      res.json({"times":req.session.page_views});
+   if(times[req.session]){
+      times[req.session] += 1;
+      res.json({"times":times[req.session]});
    } else {
-      req.session.page_views = 1;
-	  
-	  req.session.save();
-      res.json({"times":req.session.page_views});
+     times[req.session] = 1;
+      res.json({"times":times[req.session]});
    }
 });
 
