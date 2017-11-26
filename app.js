3a09e8f500c8470f50f566732fe2333f85db1f76
leo ouyang <leo.ouyang@mail.utornto.ca>
2017-11-25 22:41:13 -0500

    testing session

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-var app = express();
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 app.use(cookieParser());
 app.use(session({secret: "CSC309"}));
@@ -17,6 +18,6 @@ app.get('/', function(req, res){
    }
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', app.address().port);
+app.listen(PORT, function(){
+  console.log('listening on', PORT);
 });
